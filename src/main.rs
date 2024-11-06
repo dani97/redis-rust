9beb698ed445da2390252c9ac2546f41160ed9a0
Christopher Daniel <cdaniel@adobe.com>
2024-11-06 12:09:42 +0530

    echo and command

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,47 @@ use std::{
 };
 
 fn handle_connection(mut stream: TcpStream) {
-    // let mut buf_reader = BufReader::new(&mut stream);
-    // let mut line = String::new();
-    // let mut no_of_bytes = 0;
-    // let mut commands: Vec<String> = Vec::new();
-    // buf_reader.read_line(&mut line).unwrap();
-    
-    // for _ in 0..6 {
-    //     line.clear();
-    //     no_of_bytes = buf_reader.read_line(&mut line).unwrap();
-    //     line = line.trim().to_owned();
-    //     println!("{}", line);
-    //     println!("{}", no_of_bytes);
-    //     if let Some(command) = commands_list.get(&line) {
-    //         commands.push(command.to_string());
-    //     }
-    // }
-
-    //println!("{:?}", commands);
-    let mut buf = [0; 512];
     loop {
-        
-        let no_of_bytes = stream.read(&mut buf).unwrap();
+        let mut buf_reader = BufReader::new(&mut stream);
+        let mut line = String::new();
+        let no_of_bytes = buf_reader.read_line(&mut line).unwrap();
         if no_of_bytes == 0 {
             break;
         }
-        stream.write(b"+PONG\r\n").unwrap();
+        let no_of_commands = line.chars().nth(1).unwrap().to_digit(10).unwrap();
+        let mut echo_flag = false;
+        let mut commands: Vec<String> = Vec::new();
+        println!("{}", no_of_commands);
+        for _ in 0..no_of_commands {
+            line.clear();
+            buf_reader.read_line(&mut line).unwrap();
+            line.clear();
+            buf_reader.read_line(&mut line).unwrap();
+            line = line.trim().to_owned();
+            commands.push(line.clone());
+        }
+
+        println!("{:?}", commands);
+        for command in commands {
+            println!("{}", command);
+            if echo_flag {
+                let mut response = String::new();
+                response.push_str("+");
+                response.push_str(&command);
+                response.push_str("\r\n");
+                println!("{}", response);
+                stream.write(response.as_bytes()).unwrap();
+                echo_flag = false;
+            }
+            if command.contains("PING") {
+                stream.write(b"+PONG\r\n").unwrap();
+            } else if command.contains("ECHO") {
+                echo_flag = true;
+            }
+            if command.contains("COMMAND") {
+                stream.write(b"$0\r\n\r\n").unwrap();
+            }
+        }
     }
 }
 
