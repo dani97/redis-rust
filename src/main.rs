75ba7d34993d7760084d3db0f0209e699878a6f8
Christopher Daniel <cdaniel@adobe.com>
2024-11-06 10:14:23 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,41 @@
 #![allow(unused_imports)]
 use std::{
-    fs::{self, read},
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}, process::Command,
+    collections::HashSet, fs::{self, read}, hash::Hash, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, process::Command
 };
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buf_reader = BufReader::new(&mut stream);
-    let mut line = String::new();
-    let mut no_of_bytes = 0;
-    for _ in 1..4 {
-        line.clear();
-        no_of_bytes = buf_reader.read_line(&mut line).unwrap();
-        println!("{}", line);
-        println!("{}", no_of_bytes);
-    }
-    if line.eq("PING\r\n") {
+fn handle_connection(mut stream: TcpStream, commands_list: &HashSet<String>) {
+    // let mut buf_reader = BufReader::new(&mut stream);
+    // let mut line = String::new();
+    // let mut no_of_bytes = 0;
+    // let mut commands: Vec<String> = Vec::new();
+    // buf_reader.read_line(&mut line).unwrap();
+    
+    // for _ in 0..6 {
+    //     line.clear();
+    //     no_of_bytes = buf_reader.read_line(&mut line).unwrap();
+    //     line = line.trim().to_owned();
+    //     println!("{}", line);
+    //     println!("{}", no_of_bytes);
+    //     if let Some(command) = commands_list.get(&line) {
+    //         commands.push(command.to_string());
+    //     }
+    // }
+
+    //println!("{:?}", commands);
+    let mut buf = [0; 512];
+    loop {
+        
+        let no_of_bytes = stream.read(&mut buf).unwrap();
+        if no_of_bytes == 0 {
+            break;
+        }
         stream.write(b"+PONG\r\n").unwrap();
-    } 
+    }
 }
 
 fn main() {
+    let mut commands: HashSet<String> = HashSet::new();
+    commands.insert("PING".to_string());
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
@@ -31,7 +46,8 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                handle_connection(stream);
+
+                handle_connection(stream, &commands);
             }
             Err(e) => {
                 println!("error: {}", e);
